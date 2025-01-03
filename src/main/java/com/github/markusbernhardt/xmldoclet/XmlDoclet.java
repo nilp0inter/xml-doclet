e5deda96629a08339ff034513a8f325eb0b56ffd
Manoel Campos <manoelcampos@gmail.com>
2025-01-03 19:23:21 -0300

    Format TODOs
    Signed-off-by: Manoel Campos <manoelcampos@gmail.com>

--- src/main/java/com/github/markusbernhardt/xmldoclet/XmlDoclet.java
+++ src/main/java/com/github/markusbernhardt/xmldoclet/XmlDoclet.java
@@ -26,16 +26,16 @@ import java.util.logging.Logger;
  *      "https://docs.oracle.com/en/java/javase/21/docs/api/jdk.javadoc/jdk/javadoc/doclet/package-summary.html">Doclet
  *      API</a>
  * @see <a href="https://openjdk.org/groups/compiler/using-new-doclet.html">Using the new JDK 9
- *      Doclet API (refined in JDK 13)</a> TODO:
- *      <a href="https://chatgpt.com/c/675c3ead-dbc8-800a-bac5-46df2b61bef3">JDK 13 Doclet API
- *      migration</a> TODO: <a href=
- *      "https://stackoverflow.com/questions/77082583/migrating-to-java-17-how-to-get-gradle-to-generate-java-classes-from-xsd">JAXB
- *      XJC Gradle JDK 17</a> TODO:
- *      https://stackoverflow.com/questions/70423036/cxf-codegen-plugin-in-gradle TODO:
- *      https://www.baeldung.com/gradle-build-to-maven-pom TODO: Promissing (there is a PR for JDK
- *      21): https://github.com/qaware/xsd2java-gradle-plugin
+ *      Doclet API (refined in JDK 13)</a>
  */
 public class XmlDoclet implements Doclet {
+    /*
+     * TODO: <a href="https://chatgpt.com/c/675c3ead-dbc8-800a-bac5-46df2b61bef3">JDK 13 Doclet API migration</a>
+     * TODO: <a href="https://stackoverflow.com/questions/77082583/migrating-to-java-17-how-to-get-gradle-to-generate-java-classes-from-xsd">JAXB XJC Gradle JDK 17</a>
+     * TODO: https://stackoverflow.com/questions/70423036/cxf-codegen-plugin-in-gradle
+     * TODO: https://www.baeldung.com/gradle-build-to-maven-pom
+     * TODO: Promissing (there is a PR for JDK 21): https://github.com/qaware/xsd2java-gradle-plugin
+     */
     private static final Logger LOGGER = Logger.getLogger(XmlDoclet.class.getName());
 
     public static final String RESTRUCTURED_XSL = "/com/manticore/xsl/restructured.xsl";
@@ -54,7 +54,7 @@ public class XmlDoclet implements Doclet {
 
     /**
      * Set of supported Doclet options, generated from the Apache Commons CLI Options.
-     * 
+     *
      * @see #cliOptions
      */
     private final Set<CustomOption> options;
