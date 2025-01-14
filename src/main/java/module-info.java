05d5ed6870a6c2221a65b133988cdc1a86f95dc3
Manoel Campos <manoelcampos@gmail.com>
2025-01-13 23:14:37 -0300

    Remove module-info.java and the configuration on build.gradle
    Signed-off-by: Manoel Campos <manoelcampos@gmail.com>

--- src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module xml.doclet.main {
-    requires jakarta.xml.bind;
-    requires java.logging;
-    requires jdk.javadoc;
-    requires Saxon.HE;
-    requires org.apache.commons.cli;
-
-    exports com.github.markusbernhardt.xmldoclet;
-}
