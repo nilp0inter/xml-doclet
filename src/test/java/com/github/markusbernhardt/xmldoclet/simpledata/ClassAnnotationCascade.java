ae9ca0e064a02f9ef617b35e2f53896230446e6b
Manoel Campos <manoelcampos@gmail.com>
2025-01-30 19:35:29 -0300

    Format ClassAnnotationCascade
    Signed-off-by: Manoel Campos <manoelcampos@gmail.com>

--- src/test/java/com/github/markusbernhardt/xmldoclet/simpledata/ClassAnnotationCascade.java
+++ src/test/java/com/github/markusbernhardt/xmldoclet/simpledata/ClassAnnotationCascade.java
@@ -5,15 +5,15 @@ package com.github.markusbernhardt.xmldoclet.simpledata;
  */
 @AnnotationCascade(
         children = {
-                @AnnotationCascadeChild(name = "primitive",
-                        dummyData = {"A", "B", "C"}),
+                @AnnotationCascadeChild(name = "primitive", dummyData = {"A", "B", "C"}),
                 @AnnotationCascadeChild(name = "nested",
                         subAnnotations = {
                                 @Annotation3(id = 4),
                                 @Annotation3(id = 5),
                                 @Annotation3(id = 666)
 
-                        })
+                        }
+                )
         })
 public class ClassAnnotationCascade implements Interface2 {
 
